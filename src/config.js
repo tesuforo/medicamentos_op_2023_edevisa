590e8110f545f3a7ebfd05b995fd3ca31db94399
tesuforo <javiergarciaruiz@gmail.com>
2020-11-29 16:56:47 -0500

    swaguere

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || '89javier',
+        user: process.env.DATABASE_USER || 'rootlinks',
+        password: process.env.DATABASE_PASSWORD || 'Passdblinks123',
         database: process.env.DATABASE_NAME || 'db_links'
     },
     port: process.env.PORT || 2000
