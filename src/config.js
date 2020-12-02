47328e67ac326341b32db0096f36b481f5a923f3
tesuforo <javiergarciaruiz@gmail.com>
2020-12-02 11:08:45 -0500

    Javier

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
         connectionLimit: 10,
         host: process.env.DATABASE_HOST || 'localhost',
         user: process.env.DATABASE_USER || 'rootlinks',
-        password: process.env.DATABASE_PASSWORD || '89javier',
-        database: process.env.DATABASE_NAME || 'Passdblinks123'
+        password: process.env.DATABASE_PASSWORD || 'Passdblinks123',
+        database: process.env.DATABASE_NAME || 'db_links'
     },
-    port: process.env.PORT || 2000
+    port: process.env.PORT || 4001
 };
