b40c53c08a834a3bd5476007ba3ba284fd223765
tesuforo <javiergarciaruiz@gmail.com>
2021-05-31 16:52:06 -0500

    modules -3

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER || 'rootdblink',
+        user: process.env.DATABASE_USER || 'root',
         password: process.env.DATABASE_PASSWORD || '89javier',
         database: process.env.DATABASE_NAME || 'DBLINK'
     },
